517d910336b692dbd6ce950cc0561c196f4cc674
xiansakana <2461298052@qq.com>
2024-04-04 17:20:17 -0400

    update hero headers

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -8,24 +8,20 @@ const Hero = () => {
       />
       <div
         id="#home"
-        className="absolute left-1/2 pl-4 sm:pl-12 lg:pl-16 h-full w-1/2 top-0 flex flex-col justify-center items-start sm:gap-y-1 md:gap-y-2 lg:gap-y-4"
+        className="absolute left-1/2 pl-4 sm:pl-12 lg:pl-16 h-full w-1/2 top-0 p-4 flex flex-col justify-center items-start sm:gap-y-1 md:gap-y-2 lg:gap-y-12"
       >
-        <div className="font-bold text-left text-black text-[12px]  sm:text-xl md:text-2xl lg:text-4xl">
-          <span className="text-green-800">Learn </span> Farming Practices
-          <br />
-          <span className="text-green-800">Always.</span>
+        <div className="font-bold text-left text-black text-[12px] sm:text-xl md:text-2xl lg:text-5xl flex flex-col gap-y-2">
+          <span className="text-base text-green-700">WELCOME TO GROWGUIDE</span>
+          <span className="text-green-800">Learn Farming</span>
+          <span className="text-green-800">Practices</span>
+          <span className="text-green-800">Visit the Market</span>
+          <span className="text-green-800">and More...</span>
         </div>
-        <p className="text-black font-bold text-[10px] md:text-sm lg:text-base ">
-          With GrowGuide
-        </p>
-        <p className="text-black text-[10px] md:text-sm lg:text-base">
-          Visit the Shop and More...
-        </p>
-        <div className="flex flex-wrap md:gap-4 gap-1">
-          <button className="flex mt-1 px-1 sm:px-4 md:px-8 py-1 sm:py-2 bg-orange-500 text-white rounded-lg shadow-xl hover:bg-green-600 transition-colors ">
+        <div className="flex md:flex-row flex-col flex-wrap md:gap-4 gap-1">
+          <button className="flex mt-1 px-1 sm:px-4 md:px-8 lg:px-16 py-1 sm:py-2 lg:py-4 bg-orange-500 text-white rounded-lg shadow-xl hover:bg-green-600 transition-colors ">
             <p className="text-[8px] md:text-sm lg:text-base">Visit the Shop</p>
           </button>
-          <button className="flex mt-1 px-1 sm:px-4 md:px-8 py-1 sm:py-2 bg-orange-500 text-white rounded-lg shadow-xl hover:bg-green-600 transition-colors ">
+          <button className="flex mt-1 px-1 sm:px-4 md:px-8 lg:px-16 py-1 sm:py-2 lg:py-4 bg-orange-500 text-white rounded-lg shadow-xl hover:bg-green-600 transition-colors ">
             <a href="#download">
               <p className="text-[8px] md:text-sm lg:text-medium">
                 Download App
